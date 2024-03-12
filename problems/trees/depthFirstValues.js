94d3691b7433df1f9742807c880296ce3d657cb0
David <dmaine6711@gmail.com>
2024-03-12 10:49:02 -0400

    solve depth first search iteratively

--- problems/trees/depthFirstValues.js
+++ problems/trees/depthFirstValues.js
@@ -10,3 +10,21 @@ const depthFirstValues = (root) => {
   return [root.val, ...left, ...right]
   
 };
+
+
+
+const depthFirstValues2 = (root) => {
+  // todo
+  if(!root) return []
+  const stack = [root]
+  const result= []
+  
+  while(stack.length) {
+    let current = stack.pop()
+    result.push(current.val)
+    if (current.right) stack.push(current.right)
+    if (current.left) stack.push(current.left)  
+  }
+  
+  return result 
+};
