8a699ae3bfc9452480e381e9110ce8520ad9f541
David <dmaine6711@gmail.com>
2024-03-20 16:09:35 -0400

    sovle count islands iteratively

--- problems/graphs/countIslands.js
+++ problems/graphs/countIslands.js
@@ -28,3 +28,51 @@ const explore = (location, visited, grid) => {
   explore([x, y-1], visited, grid)
   return true  
 }
+
+
+
+//iterative: 
+
+const islandCountIterative = (grid) => {
+  // todo
+  let visited = new Set();
+  let count = 0
+  for(let i = 0; i < grid.length; i++) {
+    for(let j = 0; j < grid[0].length; j++) {
+        if (exploreIteverative([i, j], visited, grid) === true) count+=1
+    }
+  }
+  return count  
+};
+
+const exploreIteverative = (location, visited, grid) => {
+  let [x, y] = location 
+  if(grid[x][y] === 'W') return false 
+  let pos = x + ',' + y
+  if(visited.has(pos)) return false 
+
+  
+  let stack = [location]
+  
+  while(stack.length) {
+    let current = stack.pop()
+    let [x, y] = current 
+    const rowInbounds = 0 <= x && x < grid.length;
+    const colInbounds = 0 <= y && y < grid[0].length;
+    if (!rowInbounds || !colInbounds) continue;
+
+    if(grid[x][y] === 'W') continue;  
+
+    pos = x + ',' + y
+    if(visited.has(pos)) continue  
+    visited.add(pos)
+
+   stack.push([x+1, y])
+   stack.push([x-1, y])
+   stack.push([x, y+1])
+   stack.push([x, y-1])
+    
+  }
+
+  return true  
+}
