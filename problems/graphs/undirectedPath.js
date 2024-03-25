bf9c5a3fab31dd26ad2f917f688bc0eb886346c6
David <dmaine6711@gmail.com>
2024-03-25 12:39:41 -0400

    solve undirectedPath again

--- problems/graphs/undirectedPath.js
+++ problems/graphs/undirectedPath.js
@@ -1,32 +1,70 @@
-const undirectedPath = (edges, nodeA, nodeB) => {
-  let graph = createGraph(edges);
-  console.log(graph)
-  return path(graph, nodeA, nodeB, new Set())
+// const undirectedPath = (edges, nodeA, nodeB) => {
+//   let graph = createGraph(edges);
+//   console.log(graph)
+//   return path(graph, nodeA, nodeB, new Set())
   
+// }
+
+// const path = (graph, src, dst, visited) => {
+//   if(src === dst) return true 
+//   if(visited.has(src)) return false  
+  
+//   visited.add(src)
+//   for(let neighbor of graph[src]) {
+//     if(path(graph, neighbor, dst, visited)) return true 
+//   }
+   
+//   return false 
+// }
+  
+// const createGraph = (edges) => {
+//   const graph = {}
+  
+//   for(let edge of edges) {
+//     const [a, b] = edge;
+//     if(!(a in graph)) graph[a] = []  
+//     if(!(b in graph)) graph[b] = [] 
+//     graph[a].push(b)
+//     graph[b].push(a)
+//   }
+//   return graph
+// }
+
+
+
+const undirectedPath = (edges, nodeA, nodeB) => {
+  const graph = createGraph(edges)
+  return path(graph, nodeA, nodeB, new Set()) 
+      
 }
 
 const path = (graph, src, dst, visited) => {
-  if(src === dst) return true 
-  if(visited.has(src)) return false  
   
-  visited.add(src)
-  for(let neighbor of graph[src]) {
-    if(path(graph, neighbor, dst, visited)) return true 
+  let stack = [src]
+  while(stack.length ) {
+    let currentNode = stack.pop()
+    if(currentNode === dst) return true  
+    for(let neighbor of graph[currentNode]) {
+      if(!visited.has(neighbor)) {
+        stack.push(neighbor)
+        visited.add(neighbor)
+      }
+    }
   }
-   
-  return false 
+  return false  
 }
   
 const createGraph = (edges) => {
-  const graph = {}
-  
-  for(let edge of edges) {
-    const [a, b] = edge;
-    if(!(a in graph)) graph[a] = []  
-    if(!(b in graph)) graph[b] = [] 
-    graph[a].push(b)
-    graph[b].push(a)
+  const graph = {} 
+  for (let edge of edges) {
+    const [x,y] = edge 
+    if(!graph[x]) graph[x] = []
+    graph[x].push(y)
+    
+    if(!graph[y]) graph[y] = []
+    graph[y].push(x)
   }
   return graph
 }
+  
   
