5d06abb2008dcf99016c69f0163ad412111c501f
David <dmaine6711@gmail.com>
2024-03-25 15:07:17 -0400

    solve shortestPath again

--- problems/graphs/shortestPath.js
+++ problems/graphs/shortestPath.js
@@ -1,21 +1,49 @@
-const shortestPath = (edges, nodeA, nodeB) => {
+// const shortestPath = (edges, nodeA, nodeB) => {
   
-  let graph = buildGraph(edges)
-  let queue = [[nodeA, 0]]
-  let visited = new Set([nodeA])
+//   let graph = buildGraph(edges)
+//   let queue = [[nodeA, 0]]
+//   let visited = new Set([nodeA])
 
+//   while(queue.length) {
+
+//     let [current, distance] = queue.shift()
+
+//     if(current === nodeB) return distance 
+//     for(let neighbor of graph[current]) {
+//       if(!visited.has(neighbor)) {
+//         visited.add(neighbor)
+//         queue.push([neighbor, distance+1])
+//       }
+
+//     }
+//   }
+//   return -1
+// };
+
+
+const shortestPath = (edges, nodeA, nodeB) => {
+  const graph = createGraph(edges)
+  const visited = new Set()
+
+  
+  return explore(graph, nodeA, nodeB, visited)  
+
+
+};
+
+
+const explore = (graph, src, target, visited) => {
+  
+  let queue = [[src, 0]]
   while(queue.length) {
-
-    let [current, distance] = queue.shift()
-
-    if(current === nodeB) return distance 
+    let [current, count] = queue.shift()
+    if(current===target) return count
+    visited.add(current)
     for(let neighbor of graph[current]) {
       if(!visited.has(neighbor)) {
-        visited.add(neighbor)
-        queue.push([neighbor, distance+1])
+        queue.push([neighbor, count+1])
       }
-
     }
   }
   return -1
-};
+}
