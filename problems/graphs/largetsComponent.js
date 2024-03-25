ef0b5ee6c2f6ffda9a8d12802f80ec35a77873d3
David <dmaine6711@gmail.com>
2024-03-25 14:38:07 -0400

    solve largestComponent again

--- problems/graphs/largetsComponent.js
+++ problems/graphs/largetsComponent.js
@@ -1,24 +1,53 @@
-const largestComponent = (graph) => {
-  // todo
-  let largest = 0
-  let countSet = new Set()
-  for(let node in graph) {
+// const largestComponent = (graph) => {
+//   // todo
+//   let largest = 0
+//   let countSet = new Set()
+//   for(let node in graph) {
    
-    let subCount = explore(graph, node, countSet)
-    largest = Math.max(largest, subCount)
+//     let subCount = explore(graph, node, countSet)
+//     largest = Math.max(largest, subCount)
+//   }
+//   return largest
+// };
+
+
+// const explore = (graph, node, countSet) => {
+//   if(countSet.has(node)) return 0 
+  
+//   countSet.add(node)
+    
+//   let size = 1
+//   for(let neighbor of graph[node]){
+//     size += explore(graph,neighbor, countSet)
+//   }
+//  return size  
+// }
+
+const largestComponent = (graph) => {
+  let visited = new Set()
+  let max = 0
+  for(let node in graph) {
+    let result = explore(graph, node, visited)
+    max = Math.max(result, max)
+    
   }
-  return largest
+  return max 
+  
 };
 
 
-const explore = (graph, node, countSet) => {
-  if(countSet.has(node)) return 0 
+//visited: 0, 8, 5  
+
+const explore = (graph, node, visited) => {
+  if(visited.has(String(node))) return 0 
   
-  countSet.add(node)
-    
+  visited.add(String(node))
+  
   let size = 1
-  for(let neighbor of graph[node]){
-    size += explore(graph,neighbor, countSet)
+  for(let neighbor of graph[node]) {
+    if(!visited.has(String(node))) {
+      size += explore(graph, neighbor, visited) 
+    }
   }
- return size  
+  return size 
 }
