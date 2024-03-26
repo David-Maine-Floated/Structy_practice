784dbff390ea937a6e6da7f2168872d446f53e38
David <dmaine6711@gmail.com>
2024-03-26 11:25:23 -0400

    solve minimum island count again

--- problems/graphs/minimumIsland.js
+++ problems/graphs/minimumIsland.js
@@ -1,32 +1,68 @@
+// const minimumIsland = (grid) => {
+//   let visited = new Set()
+//   let smallest = Infinity
+//   for(let r = 0; r < grid.length; r++) {
+//     for(let c = 0; c < grid[0].length; c++) {
+//       let result = explore(grid, r, c, visited)
+//       if (result> 0 && result< smallest) {
+//         smallest = result;
+//       }
+//     }
+//   }
+//   return smallest
+// };
+
+// const explore = (grid, r, c, visited) => {
+//   if(r < 0 || r >= grid.length) return 0
+//   if (c < 0 || c >= grid[0].length) return 0
+    
+//   if(grid[r][c] === "W") return 0
+  
+//   let pos = r + ',' + c;
+//   if(visited.has(pos)) return 0 
+//   visited.add(pos)
+//   let count = 1
+  
+//   count += explore(grid, r+1, c, visited) 
+//   count += explore(grid, r-1, c, visited)
+//   count += explore(grid, r, c+1, visited)
+//   count += explore(grid, r, c-1, visited)
+  
+//   return count 
+// }
+
+
 const minimumIsland = (grid) => {
-  let visited = new Set()
-  let smallest = Infinity
+  
+  const visited = new Set()
+  let min = Infinity
   for(let r = 0; r < grid.length; r++) {
     for(let c = 0; c < grid[0].length; c++) {
-      let result = explore(grid, r, c, visited)
-      if (result> 0 && result< smallest) {
-        smallest = result;
-      }
+      let result = explore(grid, r, c, visited) 
+      if(result !== 0 )min = Math.min(result, min)
     }
   }
-  return smallest
+  return min 
 };
 
 const explore = (grid, r, c, visited) => {
-  if(r < 0 || r >= grid.length) return 0
-  if (c < 0 || c >= grid[0].length) return 0
-    
-  if(grid[r][c] === "W") return 0
-  
-  let pos = r + ',' + c;
-  if(visited.has(pos)) return 0 
+  let pos = r + ',' + c
+  if(visited.has(pos) || outOfBounds(grid,r,c) || grid[r][c] === "W") return 0
   visited.add(pos)
-  let count = 1
   
-  count += explore(grid, r+1, c, visited) 
-  count += explore(grid, r-1, c, visited)
-  count += explore(grid, r, c+1, visited)
-  count += explore(grid, r, c-1, visited)
+  let length = 1
   
-  return count 
+  length += explore(grid, r+1, c, visited)
+  length += explore(grid, r-1, c, visited)
+  length += explore(grid, r, c+1, visited)
+  length += explore(grid, r, c-1, visited)
+
+  
+  return length  
+} 
+
+const outOfBounds = (grid, r,c) => {
+  if(r < 0 || r >= grid.length) return true  
+  if(c < 0 || c >= grid[0].length) return true 
+  return false 
 }
