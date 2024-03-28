522a83238e120c96d36c0ac5a288c267945383a2
David <dmaine6711@gmail.com>
2024-03-28 14:10:32 -0400

    solve sememsteres Required again

--- problems/graphs/semestersRequired.js
+++ problems/graphs/semestersRequired.js
@@ -1,43 +1,42 @@
+
 const semestersRequired = (numCourses, prereqs) => {
-  if(!prereqs.length) return 1
-  // todo 
-  let graph = createGraph(numCourses, prereqs)
-  console.log(graph)
-  let count = {}
-  let max = 0
+  const graph = createGraph(prereqs, numCourses);
+  let count = {} 
+  
   for(let node in graph) {
-    let result = explore(graph, node, count)
-    max = Math.max(max, result)
+    if(graph[node].length === 0) count[node] = 1 
   }
-  return max 
+  
+  for(let node in graph) {
+    explore(graph, node, count)
+  }
+  
+  return Math.max(...Object.values(count))
+  
 };
 
-
 function explore(graph, node, count) {
-  
-  if(!graph[node].length) return 0 
-  if(count[node]) return count[node]
-  
-  let semesters = 1
-  for(let neighor of graph[node]) {
-    let result = explore(graph, neighor, count)
-    semesters = Math.max(result, semesters)
+  if (count[node]) return count[node] 
+  // Base case: no prerequisites
+  let max = 0;
+  for (let neighbor of graph[node]) {
+    let result = explore(graph, neighbor, count); // Increment depth by 1
+    max = Math.max(result, max);
   }
-    
-  count[node] = semesters + 1
+  
+  count[node] = max + 1
   return count[node]
 }
 
-function createGraph(numcourses, prereqs) {
-  let graph = {}
-  for(let pair of prereqs) {
-    [a,b] = pair 
-    if(!graph[b]) graph[b] = [];
-    graph[b].push(a)
+function createGraph(prereqs, numCourses) {
+  let graph = {};
+  
+  for(let i = 0; i < numCourses; i++) {
+    graph[i] = []
   }
-  for(let i = 0; i < numcourses; i++) {
-    if(!graph[i]) graph[i] = []
-    
+  for (let edge of prereqs) {
+    let [x, y] = edge;
+    graph[x].push(y); // Fix: Push y, not x
   }
-  return graph  
+  return graph;
 }
