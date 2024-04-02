15c441e1b49b51b2c1821b7eb45a8ba51d6d4799
David <dmaine6711@gmail.com>
2024-04-02 15:51:35 -0400

    solve min change recusrivley

--- problems/dynamic_programming/minChange.js
+++ problems/dynamic_programming/minChange.js
@@ -19,3 +19,26 @@ const minChange = (amount, coins) => {
 
   return -1; // If no exact change found
 };
+
+
+const minChangeRecursvie = (amount, coins) => {
+
+  const answer = _minChange(amount, coins);
+  return answer === Infinity ? -1 : answer;
+}
+
+const _minChange = (amount, coins, memo = {}) => {
+
+  if(amount in memo) return memo[amount]
+  if(amount === 0) return 0 
+  
+  if(amount < 0) return Infinity;
+
+  let minCoins = Infinity 
+  for(let coin of coins) {
+     const numCoins = _minChange(amount - coin, coins, memo) + 1
+      minCoins = Math.min(minCoins, numCoins)
+  }
+  memo[amount] = minCoins 
+  return minCoins 
+};
