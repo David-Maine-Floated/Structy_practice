24dbfedc340a2efe71dfae47b44fc846b0761534
David <dmaine6711@gmail.com>
2024-04-30 14:35:31 -0400

    solve create combinations

--- problems/recursivee/createCombinations.js
+++ problems/recursivee/createCombinations.js
@@ -1,29 +1,19 @@
-// const createCombinations = (items, k) => {
-//   if(k === 0) return [[]]
-//   if(k > items.length) return []
+const createCombinations = (items, k) => {
+  if(k === 0) return [[]]
+  if( k > items.length) return []
+  
+  const first = items[0]
 
-//   const first = items[0]
-
-//   let itemsWithFirst = []
-//   let addToFirst = createCombinations(items.slice(1), k-1)
-
-//   for(let item of addToFirst) {
-//     itemsWithFirst.push([first, ...item])
-//   }
-
-//   let noFirst = createCombinations(items.slice(1), k)
-
-//   return [...itemsWithFirst, ...noFirst]
-// };
+  const partialCombos = createCombinations(items.slice(1), k-1) 
+  const combostWithoutFirst = createCombinations(items.slice(1), k)
 
 
-const x = 2; 
-let y = 4;
+  const combosWithFirst = []
+  for(let partialCombo of partialCombos) {
+    combosWithFirst.push([first, ...partialCombo])
+  }
 
-function update(arg) { 
-  return Math.random() + y * arg; 
-} 
-y = 2; 
-y = 3
-result = update(x); 
-console.log(result)
+  return [...combostWithoutFirst, ...combosWithFirst]
+  
+
+};
