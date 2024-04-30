fccdb40371f609e5b7bd929ff8e3525d7c1579f5
David <dmaine6711@gmail.com>
2024-04-30 11:28:11 -0400

    solve paired parenthesis

--- problems/stack/pairedParenthesis.js
+++ problems/stack/pairedParenthesis.js
@@ -1,18 +1,16 @@
 const pairedParentheses = (str) => {
-  // todo
   let stack = []
-  for (let char of str) {
-    switch(char) {
-      case '(':
-        stack.push('(');
-        break;
-      case ')':
-        console.log(stack)
-        let top = stack.pop()
-        if(top !== '(') return false 
-        break;
-    }   
+  let i = 0;
+  while(i < str.length) {
+    if(str[i] === '(') {
+      stack.push('(')
+    } else if(str[i] === ')') {
+      if(stack.length === 0) return false 
+      stack.pop()
+    }
+    i++
   }
-  if(stack.length) return false 
+  if(stack.length !== 0) return false 
   return true 
+  
 };
