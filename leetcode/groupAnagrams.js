632b3044f2d2d81a19f19615e6bf2c5b71b2ed8b
David <dmaine6711@gmail.com>
2024-05-09 15:29:43 -0400

    solve groupAnagrams without sorting

--- leetcode/groupAnagrams.js
+++ leetcode/groupAnagrams.js
@@ -1,11 +1,49 @@
-var groupAnagrams = function(strs) {
-    const count = {}
-    for(let str of strs) {
-        const sorted = str.split("").sort().join('')
-        if(!(sorted in count)) count[sorted] = [];
+// var groupAnagrams = function(strs) {
+//     const count = {}
+//     for(let str of strs) {
+//         const sorted = str.split("").sort().join('')
+//         if(!(sorted in count)) count[sorted] = [];
  
-        count[sorted].push(str)
+//         count[sorted].push(str)
+//     }
+
+//     return Object.values(count)
+// };
+
+
+var getSignature = function(s) {
+    const count = Array(26).fill(0);
+    for (const c of s) {
+        count[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
     }
 
-    return Object.values(count)
+    const result = [];
+    for (let i = 0; i < 26; i++) {
+        if (count[i] !== 0) {
+            result.push(String.fromCharCode(i + 'a'.charCodeAt(0)), count[i].toString());
+        }
+    }
+
+    return result.join('');
+};
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const result = [];
+    const groups = new Map();
+
+    for (const s of strs) {
+        const signature = getSignature(s);
+        if (!groups.has(signature)) {
+            groups.set(signature, []);
+        }
+        groups.get(signature).push(s);
+    }
+
+    groups.forEach(value => result.push(value));
+
+    return result;
 };
