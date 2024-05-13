d483cd8715dac1daec99dae67dd8e76aca1113f1
David <dmaine6711@gmail.com>
2024-05-13 14:09:26 -0400

    sovle product of array besides index

--- leetcode/productOfArrayBesidesSelf.js
+++ leetcode/productOfArrayBesidesSelf.js
@@ -22,7 +22,6 @@ var productExceptSelf = function(nums) {
         if(i===0) {
             result.push(suffix[i+1])
         } else if(i===nums.length - 1) {
-            console.log('hehe hello!')
             result.push(prefix[i-1])
         } else {
             result.push(prefix[i-1] * suffix[i+1])
@@ -30,4 +29,25 @@ var productExceptSelf = function(nums) {
     }
 
     return result
+}
+
+
+var productExceptSelfBetter = function(nums) {
+
+    const result = new Array(nums.length).fill(1)
+    
+    let prefix = 1
+    for (let i = 0; i < nums.length; i++) {
+
+        result[i] *= prefix
+        prefix *= nums[i] 
+    }
+    console.log(result)
+    let postfix = 1
+    for (let i = nums.length - 1; i >= 0; i--) {
+        result[i] *= postfix
+        postfix *= nums[i]
+    }
+
+    return result 
 }
