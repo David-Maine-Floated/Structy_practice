6114051d9c8680b4d5865ca5e9dedc44c1a6eeff
David <dmaine6711@gmail.com>
2024-05-30 15:09:00 -0400

    solve levelOrderTraversalBinaryTree with BFS

--- leetcode/levelOrderTraversalBinaryTree.js
+++ leetcode/levelOrderTraversalBinaryTree.js
@@ -9,21 +9,66 @@
  * }
  */
 
+
+//With Stack:
+
+// class Solution {
+//     /**
+//      * @param {TreeNode} root
+//      * @return {number[][]}
+//      */
+//     levelOrder(root) {
+//         if(!root) return []
+//         let result = []
+//         const stack = [[root, 0]]
+//         while(stack.length) {
+//             const [currentNode, index] = stack.pop();
+//             if(result[index] === undefined) result[index] = [];
+//             result[index].push(currentNode.val);
+//             if(currentNode.right) stack.push([currentNode.right, index + 1])
+//             if(currentNode.left) stack.push([currentNode.left, index + 1])
+//         }
+//         return result
+//     }
+
+// }
+
+
+//With Queue: 
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
 class Solution {
     /**
      * @param {TreeNode} root
      * @return {number[][]}
      */
     levelOrder(root) {
-        if(!root) return []
-        let result = []
-        const stack = [[root, 0]]
-        while(stack.length) {
-            const [currentNode, index] = stack.pop();
-            if(result[index] === undefined) result[index] = [];
-            result[index].push(currentNode.val);
-            if(currentNode.right) stack.push([currentNode.right, index + 1])
-            if(currentNode.left) stack.push([currentNode.left, index + 1])
+        const result = [];
+        const queue = [];
+        if (root) {
+            queue.push(root);
+        }
+        while(queue.length > 0) {
+            const subRow = []
+            const leng = queue.length;
+            for(let i = 0; i < leng; i++) {
+                const node = queue.shift();
+                subRow.push(node.val);
+                if(node.left) queue.push(node.left)
+                if(node.right) queue.push(node.right)
+                console.log(subRow)
+            } 
+            result.push(subRow)
         }
         return result
     }
