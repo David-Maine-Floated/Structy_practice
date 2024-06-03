6206557a8d32eea86740eab72aa9df17f8763b7d
David <dmaine6711@gmail.com>
2024-06-03 17:44:30 -0400

    solve with BFS

--- leetcode/serliazeDeserializeTree.js
+++ leetcode/serliazeDeserializeTree.js
@@ -59,3 +59,78 @@ class Codec {
         return dfs()
     }
 }
+
+
+//BFS :
+
+
+// /**
+//  * Definition for a binary tree node.
+//  * class TreeNode {
+//  *     constructor(val = 0, left = null, right = null) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
+
+// class Codec {
+//     /**
+//      * Encodes a tree to a single string.
+//      *
+//      * @param {TreeNode} root
+//      * @return {string}
+//      */
+//     serialize(root) {
+//         if (!root) return 'n';
+//         let res = [];
+//         const queue = [root];
+
+//         while (queue.length) {
+//             let curr = queue.shift();
+//             if (curr) {
+//                 res.push(curr.val);
+//                 queue.push(curr.left);
+//                 queue.push(curr.right);
+//             } else {
+//                 res.push('n');
+//             }
+//         }
+//         console.log(res)
+//         return res.join(',');
+//     }
+
+//     /**
+//      * Decodes your encoded data to tree.
+//      *
+//      * @param {string} data
+//      * @return {TreeNode}
+//      */
+//     deserialize(data) {
+//    if (data === 'n') return null;
+
+//         const vals = data.split(',');
+//         const root = new TreeNode(Number(vals[0]));
+//         const queue = [root];
+//         let i = 1;
+
+//         while (queue.length) {
+//             let node = queue.shift();
+
+//             if (vals[i] !== 'n') {
+//                 node.left = new TreeNode(Number(vals[i]));
+//                 queue.push(node.left);
+//             }
+//             i++;
+
+//             if (vals[i] !== 'n') {
+//                 node.right = new TreeNode(Number(vals[i]));
+//                 queue.push(node.right);
+//             }
+//             i++;
+//         }
+
+//         return root;
+//     }
+// }
